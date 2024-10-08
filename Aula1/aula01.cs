17c0e898e05d4e4bf076d2f36a99b47254a8d89d
Michelyserras <Michelysrrs@gmail.com>
2024-10-08 12:05:10 -0300

    ola mundo em c#

--- Aula1/aula01.cs
+++ Aula1/aula01.cs
@@ -1,1 +1,9 @@
-using System; //
+using System; // importando uma biblioteca que contém os comandos básicos de saída e entrada da programação;
+
+// todo programa em c# é uma classe pq é baseado em poo
+// essa estrutura básica de um programa em c#
+class Principal{
+    static void Main(string[] args){
+        Console.WriteLine("Olá Mundo! ");
+    }
+}
