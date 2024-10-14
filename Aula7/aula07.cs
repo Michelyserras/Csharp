14f9b0c26af5d9a5d03ffcdc9435fbd896badf7c
Michelyserras <Michelysrrs@gmail.com>
2024-10-14 14:44:10 -0300

    constantes

---
+++ Aula7/aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class aula7{
+    public  static void main(String[] args) {
+        // constantes, possui a mesma regra declara o tipo nome e o valor se tiver 
+        const string canal="cfb cursos";
+        const double pi= 3.14;
+
+        Console.WriteLine("Canal {0}", canal);
+
+    }
+}
